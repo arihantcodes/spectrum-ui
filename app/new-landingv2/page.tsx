8a45a4428ccdc1f0e8de09f8d34bd74e0d84e6cc
amriteshx1 <amriteshx18@gmail.com>
2025-07-12 17:30:46 +0530

    implemented hero section with react3d logo

--- app/new-landingv2/page.tsx
+++ app/new-landingv2/page.tsx
@@ -1,9 +1,28 @@
 
+import { Icons } from '@/components/icon';
+import { NumberTicker } from '@/components/magicui/number-ticker';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Image from "next/image";
 
 const LandingPage = () => {
+    // const [star, setStar] = useState(0);
+    //   const fetchGithubData = () => {
+    //     axios
+    //       .get('https://api.github.com/repos/arihantcodes/spectrum-ui')
+    //       .then((response) => {
+    //         const star = response.data.stargazers_count;
+    //         setStar(star);
+    //       })
+    //       .catch((error) => {});
+    //   };
+    
+    //   useEffect(() => {
+    //     fetchGithubData();
+    //   }, []);
   return (
-    <section className="relative h-screen w-full bg-black text-white overflow-hidden">
+    <>
+    <section className="relative min-h-screen w-full bg-black text-white overflow-auto">
       {/* Background image */}
       <Image
         src="/landingPageBg.png" 
@@ -16,7 +35,7 @@ const LandingPage = () => {
       {/* Content */}
       <div className="relative z-10 flex flex-col justify-start items-center h-full">
         {/* Navbar */}
-        <div className="h-[10vh] flex justify-center items-center gap-[3vh]">
+        <div className="h-[10vh] flex justify-center items-center gap-[3vh] mt-[1vh]">
             <div className="h-[60%] flex justify-center items-center border-2 border-gray-800 rounded-l-3xl pr-[1vh] pl-[1.5vh] py-[1vh]">
               <img
               src="/spectrumLogo.svg"
@@ -37,12 +56,42 @@ const LandingPage = () => {
                   className="text-black flex justify-center items-center px-[2.5vh] py-[1vh] rounded-3xl">
                   Book a Call
                 </button>
-
             </div>
-
         </div>
+
+        {/* Hero */}
+        <div className="h-[50vh] flex flex-col justify-start items-center gap-[4vh] mt-[12vh]">
+            <p className='text-6xl font-bold bg-[linear-gradient(90deg,_#FFFFFF_34.13%,_#2388FF_77.88%)] bg-clip-text text-transparent'>Premium UI Blocks</p>
+            <p className='text-6xl font-bold bg-[linear-gradient(90deg,_#FFFFFF_34.13%,_#2388FF_77.88%)] bg-clip-text text-transparent'>For SaaS & Startups</p>
+
+            <div className='flex justify-center items-center gap-[5vh] mt-[5vh]'>
+                <button
+                 style={{
+                    background: 'linear-gradient(90deg, #FFFFFF 39.42%, #2388FF 100%)',
+                  }}
+                 className='text-black flex justify-center items-center px-[3vh] py-[1.25vh] rounded-3xl'
+                 >
+                 Explore Components
+                </button>
+                <button className='flex bg-[rgba(38,38,38,0.6)] px-[3vh] py-[1.25vh] rounded-3xl'>
+                  <div className="flex items-center">
+                    <Icons.gitHub className="size-4" />
+    
+                    <span className="ml-1 ">Star on GitHub</span>
+                  </div>
+                  <div className="ml-1 flex items-center gap-1 text-sm md:flex">
+                    🌟 660
+                  </div>
+                </button>
+            </div>
+        </div>
+
+        {/*React3D Logo */}
+        <img src="/react3dLogo.jpg" alt="React3d Logo" className='h-32 mt-[10vh]' />
       </div>
     </section>
+
+    </>
   );
 };
 
