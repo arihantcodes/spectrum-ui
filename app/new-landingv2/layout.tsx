5c3cc4af1620573ddc8c2af37d9b9e56b5ecce28
amriteshx1 <amriteshx18@gmail.com>
2025-07-14 15:35:10 +0530

    add fonts to the layout

--- app/new-landingv2/layout.tsx
+++ app/new-landingv2/layout.tsx
@@ -1,5 +1,15 @@
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "@/app/globals.css"; // ✅ still needed for Tailwind and custom layers
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
 export const metadata = {
-  title: "Landing Page",
+  title: "New Landing Page",
 };
 
 export default function LandingLayout({
@@ -9,7 +19,7 @@ export default function LandingLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${GeistSans.variable} ${GeistMono.variable} ${inter.variable} font-regular`}>{children}</body>
     </html>
   );
 }
