ac508438dad3e9d68ea6cfb4624bc934ae3bf3cf
Arihant Jain <jainari1208@gmail.com>
2025-12-31 20:31:14 +0530

    fix:iframes issue in next config

--- next.config.mjs
+++ next.config.mjs
@@ -45,10 +45,10 @@ const nextConfig = {
           key: 'X-Content-Type-Options',
           value: 'nosniff',
         },
-        {
-          key: 'X-Frame-Options',
-          value: 'DENY',
-        },
+        // {
+        //   key: 'X-Frame-Options',
+        //   value: 'DENY',
+        // },
         {
           key: 'X-XSS-Protection',
           value: '1; mode=block',
