f4ba97b708157656652d2fe967f8ace150453edd
Arihant Jain <jainari1208@gmail.com>
2026-03-22 13:35:17 +0530

    fix sitemap issue

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,20 @@
 import { MetadataRoute } from "next";
+import { siteConfig } from "@/config/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/", "/_next/", "/public/"],
-    },
-    sitemap: "https://ui.spectrumhq.in/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/", "/dashboard/", "/sign-in/"],
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+        disallow: ["/api/", "/_next/", "/dashboard/", "/sign-in/"],
+      },
+    ],
+    sitemap: `${siteConfig.url}/sitemap.xml`,
   };
 }
